f72a48b8e1d3727f51e8fb001543ca5537a1753c
ay-can <aycan.acn2020@gmail.com>
2024-03-24 15:39:39 +0100

    add basic functions for calculating numbers

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hello!");
+function add(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function mult(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return a / b;
+}
+
+console.log(add(5, 5));
+console.log(sub(5, 5));
+console.log(mult(5, 5));
+console.log(div(5, 5));
